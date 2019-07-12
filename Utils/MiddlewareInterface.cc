f1a8e28aaca927dba7d08f132feffb66dfcc1f36
Lorenzo Uplegger <uplegger@fnal.gov>
2019-07-12 15:24:40 -0500

    Fixed send and receive packets in middleware interface controller

--- Utils/MiddlewareInterface.cc
+++ Utils/MiddlewareInterface.cc
@@ -21,14 +21,14 @@ void MiddlewareInterface::initialize(void)
 		std::cout << __PRETTY_FUNCTION__ << "ERROR CAN'T CONNECT TO SERVER!"<< std::endl;
 		abort();
 	}
-	std::string readBuffer = TCPClient::sendAndReceive("Initialize");
+	std::string readBuffer = TCPClient::sendAndReceivePacket("Initialize");
 	std::cout << __PRETTY_FUNCTION__ << "DONE WITH Initialize-" << readBuffer << "-"<< std::endl;
 }
 
 //========================================================================================================================
 void MiddlewareInterface::configure(std::string calibrationName, std::string configurationFilePath)
 {
-	std::string readBuffer = TCPClient::sendAndReceive("Configure,Calibration:" + calibrationName + ",ConfigurationFile:" + configurationFilePath);
+	std::string readBuffer = TCPClient::sendAndReceivePacket("Configure,Calibration:" + calibrationName + ",ConfigurationFile:" + configurationFilePath);
 	std::cout << __PRETTY_FUNCTION__ << "DONE WITH Configure-" << readBuffer << "-"<< std::endl;
 }
 
@@ -53,7 +53,7 @@ void MiddlewareInterface::resume(void)
 //========================================================================================================================
 void MiddlewareInterface::start(std::string runNumber)
 {
-	std::string readBuffer = TCPClient::sendAndReceive("Start:{RunNumber:" + runNumber + "}");
+	std::string readBuffer = TCPClient::sendAndReceivePacket("Start:{RunNumber:" + runNumber + "}");
 	std::cout << __PRETTY_FUNCTION__ << "DONE WITH Start-" << readBuffer << "-"<< std::endl;
 }
 
@@ -61,7 +61,7 @@ void MiddlewareInterface::start(std::string runNumber)
 void MiddlewareInterface::stop(void)
 {
 	std::cout << __PRETTY_FUNCTION__ << "Sending Stop!" << std::endl;
-	std::string readBuffer = TCPClient::sendAndReceive("Stop");
+	std::string readBuffer = TCPClient::sendAndReceivePacket("Stop");
 
 	std::cout << __PRETTY_FUNCTION__ << "DONE WITH Stop-" << readBuffer << "-"<< std::endl;
 
