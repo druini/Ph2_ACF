eac583ad8a5c7796823ce1d26d3b40fd3b400ab7
Lorenzo Uplegger <uplegger@fnal.gov>
2019-07-20 12:21:01 -0500

    Improvements on NetworkUtils/TCPReceiverSocket.cc ane NetworkUtils/TCPServer.cc

--- NetworkUtils/TCPReceiverSocket.cc
+++ NetworkUtils/TCPReceiverSocket.cc
@@ -2,7 +2,7 @@
 #include <sys/socket.h>
 #include <unistd.h> //read
 #include <stdexcept>
-#include <string.h>
+#include <sstream>
 #include <iostream>
 
 //========================================================================================================================
@@ -40,28 +40,27 @@ std::size_t TCPReceiverSocket::receive(char *buffer, std::size_t bufferSize)
 	std::size_t dataRead = ::read(getSocketId(), buffer, bufferSize);
 	if (dataRead == static_cast<std::size_t>(-1))
 	{
+		std::stringstream error;
 		switch (errno)
 		{
 		case EBADF:
+			error << "Socket file descriptor " << getSocketId() << " is not a valid file descriptor or is not open for reading...Errno: " << errno;
+			break;
 		case EFAULT:
-		case EINVAL:
+			error << "Buffer is outside your accessible address space...Errno: " << errno;
+			break;
 		case ENXIO:
 		{
 			// Fatal error. Programming bug
-			throw std::domain_error(std::string("Read: critical error: ") + strerror(errno));
-		}
-		case EIO:
-		case ENOBUFS:
-		case ENOMEM:
-		{
-			// Resource acquisition failure or device error
-			throw std::runtime_error(std::string("Read: resource failure: ") + strerror(errno));
+			error << "Read critical error caused by a programming bug...Errno: " << errno;
+			throw std::domain_error(error.str());
 		}
 		case EINTR:
 			// TODO: Check for user interrupt flags.
 			//       Beyond the scope of this project
 			//       so continue normal operations.
-		case ETIMEDOUT:
+			error << "The call was interrupted by a signal before any data was read...Errno: " << errno;
+			break;
 		case EAGAIN:
 		{
 			// Temporary error.
@@ -70,19 +69,21 @@ std::size_t TCPReceiverSocket::receive(char *buffer, std::size_t bufferSize)
 					  << "Read returned EAGAIN error which means that the read was temporarily broken. If this continues it is a problem because I am in a recursive loop!"
 					  << std::endl;
 			dataRead = receive(buffer, bufferSize);
+			return dataRead;
 		}
-		case ECONNRESET:
 		case ENOTCONN:
 		{
 			// Connection broken.
 			// Return the data we have available and exit
 			// as if the connection was closed correctly.
+			return dataRead;
 		}
 		default:
 		{
-			throw std::runtime_error(std::string("Read: returned -1: ") + strerror(errno));
+			error << "Read: returned -1...Errno: " << errno;
 		}
 		}
+		throw std::runtime_error(error.str());
 	}
 	//std::cout << __PRETTY_FUNCTION__ << "Message received with no errors for socket: " << getSocketId() << std::endl;
 	return dataRead;
