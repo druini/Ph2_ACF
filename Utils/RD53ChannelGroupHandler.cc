e2ee58283582271dfe43e064a5caf2e686b5b4f3
Mauro Dinardo <mauro.dinardo@cern.ch>
2019-08-15 11:08:23 +0200

    Simpler pattern

--- Utils/RD53ChannelGroupHandler.cc
+++ Utils/RD53ChannelGroupHandler.cc
@@ -22,27 +22,16 @@ void RD53ChannelGroupHandler::RD53ChannelGroupPattern::makeTestGroup (ChannelGro
 {
   static_cast<ChannelGroup*>(currentChannelGroup)->disableAllChannels();
 
-  for (int core_row = 0; core_row < NROW_CORE; core_row++)
+  for (auto col = 0u; col < Ph2_HwDescription::RD53::nCols; col++)
     {
-      for (int core_col = 0; core_col < 3; core_col++)
-	{
-	  int row_offset = (groupNumber % 64) % NROW_CORE;
-	  int col_offset = (groupNumber % 64) / NROW_CORE;
-	  int col_in_core = (core_row + col_offset) % NROW_CORE;
-
-	  for (int i = 0; i < 6; i++)
-	    {
-	      int row_in_core = (core_col + row_offset + (i % 2 == 0 ? 0 : 3)) % NROW_CORE;
-	      int row = (64 * (groupNumber / 64) + 32 * i + core_row * 8 + row_in_core) % Ph2_HwDescription::RD53::nRows;
-	      int col = 128 + 24 * i + core_col * NROW_CORE + col_in_core;
-
-	      if (isChannelEnabled(row,col)) static_cast<RD53ChannelGroupPattern*>(currentChannelGroup)->enableChannel(row, col);
-	    }
-	}
+      auto row = NROW_CORE * col;
+      row += (row/Ph2_HwDescription::RD53::nRows) % NROW_CORE + groupNumber;
+      row %= Ph2_HwDescription::RD53::nRows;
+      if (isChannelEnabled(row,col)) static_cast<RD53ChannelGroupPattern*>(currentChannelGroup)->enableChannel(row, col);
     }
 }
 
-RD53ChannelGroupHandler::RD53ChannelGroupHandler(bool doAll)
+RD53ChannelGroupHandler::RD53ChannelGroupHandler (bool doAll)
 {
   if (doAll == true)
     {
