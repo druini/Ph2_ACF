244c1640665c8727bf4feb99548fdecc7837bb4a
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2020-02-28 17:56:40 +0100

    Still trying to figure out whats going on with 2CBC3

--- System/SystemController.cc
+++ System/SystemController.cc
@@ -185,96 +185,95 @@ namespace Ph2_System
               static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->selectLink (cFe->getLinkId());
               for (auto& cReadoutChip : cFe->fReadoutChipVector)
               {
-                  /*if ( !bIgnoreI2c && cReadoutChip->getFrontEndType() == FrontEndType::CBC3 )
+                  if ( !bIgnoreI2c && cReadoutChip->getFrontEndType() == FrontEndType::CBC3 )
                   {
                     LOG (INFO) << BOLDBLUE << "Configuring readout chip [CBC" << +cReadoutChip->getChipId() << " ]" << RESET;
                     fReadoutChipInterface->ConfigureChip ( cReadoutChip );
-                    // check the first value in the register map
-                    auto cRegisterMap = cReadoutChip->getRegMap();
-                    uint16_t cRegValue = fReadoutChipInterface->ReadChipReg( cReadoutChip, cRegisterMap.begin()->first );
-                    LOG (INFO) << BOLDGREEN <<  "Successfully configured Chip " << int ( cReadoutChip->getChipId() ) << " [ " << cRegisterMap.begin()->first << " set to 0x" << std::hex << cRegValue << std::dec <<  " ]." << RESET;
+                    // // check the first value in the register map
+                    // auto cRegisterMap = cReadoutChip->getRegMap();
+                    // uint16_t cRegValue = fReadoutChipInterface->ReadChipReg( cReadoutChip, cRegisterMap.begin()->first );
+                    // LOG (INFO) << BOLDGREEN <<  "Successfully configured Chip " << int ( cReadoutChip->getChipId() ) << " [ " << cRegisterMap.begin()->first << " set to 0x" << std::hex << cRegValue << std::dec <<  " ]." << RESET;
 
-                    // if there is no CICs then do the back-end alignment here ...
-                    if( static_cast<OuterTrackerModule*>(cFe)->fCic != NULL )
-                      continue;
+                    // // if there is no CICs then do the back-end alignment here ...
+                    // if( static_cast<OuterTrackerModule*>(cFe)->fCic != NULL )
+                    //   continue;
 
-                    // original threshold
-                    uint16_t cThreshold = static_cast<CbcInterface*>(fReadoutChipInterface)->ReadChipReg(cReadoutChip, "VCth" );
-                    LOG (INFO) << BOLDBLUE << "Running phase tuning and word alignment on FE" << +cFe->getFeId() << " CBC" << +cReadoutChip->getId() << "..." << RESET;
-                    std::vector<uint8_t> cSeeds{0x35,0x6A,0xD5};
-                    uint8_t cBendCode_phAlign = 10;
-                    std::vector<uint8_t> cBendLUT = static_cast<CbcInterface*>(fReadoutChipInterface)->readLUT( cReadoutChip );
-                    auto cIterator = std::find(cBendLUT.begin(), cBendLUT.end(), cBendCode_phAlign);
-                    // if bend code isn't there ... quit
-                    if( cIterator == cBendLUT.end() )
-                        continue;
+                    // // original threshold
+                    // uint16_t cThreshold = static_cast<CbcInterface*>(fReadoutChipInterface)->ReadChipReg(cReadoutChip, "VCth" );
+                    // LOG (INFO) << BOLDBLUE << "Running phase tuning and word alignment on FE" << +cFe->getFeId() << " CBC" << +cReadoutChip->getId() << "..." << RESET;
+                    // std::vector<uint8_t> cSeeds{0x35,0x6A,0xD5};
+                    // uint8_t cBendCode_phAlign = 10;
+                    // std::vector<uint8_t> cBendLUT = static_cast<CbcInterface*>(fReadoutChipInterface)->readLUT( cReadoutChip );
+                    // auto cIterator = std::find(cBendLUT.begin(), cBendLUT.end(), cBendCode_phAlign);
+                    // // if bend code isn't there ... quit
+                    // if( cIterator == cBendLUT.end() )
+                    //     continue;
 
-                    int cPosition = std::distance( cBendLUT.begin(), cIterator);
-                    double cBend_strips = -7. + 0.5*cPosition;
-                    std::vector<int> cBends ( cSeeds.size() , static_cast<int>( cBend_strips*2));
-                    static_cast<CbcInterface*>(fReadoutChipInterface)->injectStubs( cReadoutChip , cSeeds , cBends);
-                    // make sure pT width is set to maximum
-                    static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg( cReadoutChip , "PtCut" , 14);
-                    // and that hit OR is turned off
-                    static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg( cReadoutChip , "HitOr" , 0);
-                    LOG (INFO) << BOLDBLUE << "\t.... configured CBC" << +cReadoutChip->getId() << " to disable HitOr and set PtCut to maximum value [14 half-strips]" << RESET;
-                    bool cPhaseTuningSuccess = false;
-                    uint8_t cLineId=1;
-                    for( auto& cSeed : cSeeds )
-                    {
-                        cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , cLineId++ , cSeed , 8);
-                        if (!cPhaseTuningSuccess )
-                        {
-                            LOG (INFO) << BOLDRED << "Hybrid " << +cFe->getId() << " CBC " << +cReadoutChip->getChipId() << " FAILED phase tuning on SLVS line " << +(cLineId-1) << RESET;
-                            exit(0);
-                        }
-                    }
-                    // line with bend codes
-                    cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 4 , ( cBendCode_phAlign << 4) | (cBendCode_phAlign << 0) , 8);
-                    if (!cPhaseTuningSuccess )
-                    {
-                        LOG (INFO) << BOLDRED << "Hybrid " << +cFe->getId() << " CBC " << +cReadoutChip->getChipId() << " FAILED phase tuning on last SLVS line!" << RESET;
-                        exit(0);
-                    }
-                    // final line
-                    fBeBoardInterface->ChipReSync ( cBoard );
-                    cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, (1 << 7) | (cBendCode_phAlign << 0)  , 8);
-                    if( !cPhaseTuningSuccess )
-                    {
-                        // if it doen't work then try all possible
-                        // combinations of error bits/SoF flags on
-                        LOG (INFO) << BOLDRED << "Phase Tuning/Word Alignment failed on SLVS5 : going to try and see if this is because error flags/SoF bits are high!" << RESET;
-                        cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, ( 1 << 7) | (1 <<6) | (cBendCode_phAlign <<0)  , 8);
-                        if(!cPhaseTuningSuccess)
-                        {
-                            cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, ( 1 << 7) | (1 <<6) | (1<<4) | (cBendCode_phAlign <<0)  , 8);
-                            if(! cPhaseTuningSuccess )
-                            {
-                                cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, ( 1 << 7) | (1 <<4) | (cBendCode_phAlign <<0)  , 8);
-                                if (!cPhaseTuningSuccess )
-                                {
-                                    LOG (INFO) << BOLDRED << "Hybrid " << +cFe->getId() << " CBC " << +cReadoutChip->getChipId() << " FAILED phase tuning on last SLVS line!" << RESET;
-                                    exit(0);
-                                }
-                            }
-                        }
-                    }
-                    //now unmask all channels
-                    static_cast<CbcInterface*>(fReadoutChipInterface)->MaskAllChannels( cReadoutChip, false);
-                    std::this_thread::sleep_for (std::chrono::milliseconds (50) );
-                    LOG (INFO) << BOLDBLUE << "Setting threshold back to orginal value [ " << +cThreshold << " ] DAC units." << RESET;
-                    //static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg(cReadoutChip, "VCth" , 0);
-                    //fBeBoardInterface->WriteBoardReg(cBoard, "fc7_daq_cnfg.physical_interface_block.cic.debug_select" , cReadoutChip->getChipId() ) ;
-                    //static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->L1ADebug();
-                    //static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg(cReadoutChip, "VCth" , 900);
-                    //static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->L1ADebug();
-                    
-                    static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg(cReadoutChip, "VCth" , cThreshold);
+                    // int cPosition = std::distance( cBendLUT.begin(), cIterator);
+                    // double cBend_strips = -7. + 0.5*cPosition;
+                    // std::vector<int> cBends ( cSeeds.size() , static_cast<int>( cBend_strips*2));
+                    // static_cast<CbcInterface*>(fReadoutChipInterface)->injectStubs( cReadoutChip , cSeeds , cBends);
+                    // // make sure pT width is set to maximum
+                    // static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg( cReadoutChip , "PtCut" , 14);
+                    // // and that hit OR is turned off
+                    // static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg( cReadoutChip , "HitOr" , 0);
+                    // LOG (INFO) << BOLDBLUE << "\t.... configured CBC" << +cReadoutChip->getId() << " to disable HitOr and set PtCut to maximum value [14 half-strips]" << RESET;
+                    // bool cPhaseTuningSuccess = false;
+                    // uint8_t cLineId=1;
+                    // for( auto& cSeed : cSeeds )
+                    // {
+                    //     cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , cLineId++ , cSeed , 8);
+                    //     if (!cPhaseTuningSuccess )
+                    //     {
+                    //         LOG (INFO) << BOLDRED << "Hybrid " << +cFe->getId() << " CBC " << +cReadoutChip->getChipId() << " FAILED phase tuning on SLVS line " << +(cLineId-1) << RESET;
+                    //         exit(0);
+                    //     }
+                    // }
+                    // // line with bend codes
+                    // cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 4 , ( cBendCode_phAlign << 4) | (cBendCode_phAlign << 0) , 8);
+                    // if (!cPhaseTuningSuccess )
+                    // {
+                    //     LOG (INFO) << BOLDRED << "Hybrid " << +cFe->getId() << " CBC " << +cReadoutChip->getChipId() << " FAILED phase tuning on last SLVS line!" << RESET;
+                    //     exit(0);
+                    // }
+                    // // final line
+                    // fBeBoardInterface->ChipReSync ( cBoard );
+                    // cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, (1 << 7) | (cBendCode_phAlign << 0)  , 8);
+                    // if( !cPhaseTuningSuccess )
+                    // {
+                    //     // if it doen't work then try all possible
+                    //     // combinations of error bits/SoF flags on
+                    //     LOG (INFO) << BOLDRED << "Phase Tuning/Word Alignment failed on SLVS5 : going to try and see if this is because error flags/SoF bits are high!" << RESET;
+                    //     cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, ( 1 << 7) | (1 <<6) | (cBendCode_phAlign <<0)  , 8);
+                    //     if(!cPhaseTuningSuccess)
+                    //     {
+                    //         cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, ( 1 << 7) | (1 <<6) | (1<<4) | (cBendCode_phAlign <<0)  , 8);
+                    //         if(! cPhaseTuningSuccess )
+                    //         {
+                    //             cPhaseTuningSuccess = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning( cBoard, cFe->getFeId(), cReadoutChip->getChipId() , 5, ( 1 << 7) | (1 <<4) | (cBendCode_phAlign <<0)  , 8);
+                    //             if (!cPhaseTuningSuccess )
+                    //             {
+                    //                 LOG (INFO) << BOLDRED << "Hybrid " << +cFe->getId() << " CBC " << +cReadoutChip->getChipId() << " FAILED phase tuning on last SLVS line!" << RESET;
+                    //                 exit(0);
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                    // //now unmask all channels
+                    // static_cast<CbcInterface*>(fReadoutChipInterface)->MaskAllChannels( cReadoutChip, false);
+                    // std::this_thread::sleep_for (std::chrono::milliseconds (50) );
+                    // LOG (INFO) << BOLDBLUE << "Setting threshold back to orginal value [ " << +cThreshold << " ] DAC units." << RESET;
+                    // //static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg(cReadoutChip, "VCth" , 0);
+                    // //fBeBoardInterface->WriteBoardReg(cBoard, "fc7_daq_cnfg.physical_interface_block.cic.debug_select" , cReadoutChip->getChipId() ) ;
+                    // //static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->L1ADebug();
+                    // //static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg(cReadoutChip, "VCth" , 900);
+                    // //static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->L1ADebug();
+                    // static_cast<CbcInterface*>(fReadoutChipInterface)->WriteChipReg(cReadoutChip, "VCth" , cThreshold);
                   }
-                  else*/
-                  static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning (cBoard);
-            }
+              }
           }
+          static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface())->PhaseTuning (cBoard);
+          
         }
         else
           {
