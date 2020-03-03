cc3ba5e378acfb210abcb4ee5776d8b1ef805c49
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2020-03-03 20:53:41 +0100

    Minor mods with ROH interface

--- HWInterface/ROHInterface.cc
+++ HWInterface/ROHInterface.cc
@@ -18,18 +18,10 @@ namespace Ph2_HwInterface
   void ROHInterface::ConfigureEmulator(BeBoardFWInterface* pInterface, uint32_t pPattern)
   {
     
+    LOG (INFO) << BOLDBLUE << std::bitset<32>(pPattern) << " pattern to use in PS ROH data player." << RESET;
     pInterface->WriteReg( "fc7_daq_cnfg.physical_interface_block.fe_for_ps_roh.fe_for_ps_roh_data_player_pattern", pPattern ) ;
     std::this_thread::sleep_for (std::chrono::milliseconds (500) );
       
-    if( pInterface->ReadReg("fc7_daq_stat.physical_interface_block.fe_for_ps_roh.stat_fe_for_ps_roh_data_player") == 1 )
-    {
-      fEmulatorConfigured = true;
-      LOG(INFO) << BOLDGREEN << " Data Player [CONFIGURED] " << +pPattern << RESET;
-    }
-    else
-    {
-      LOG(INFO) << BOLDRED << " Data Player [ERROR CONFIGURATION]" << RESET;
-    }
   }
 
   void ROHInterface::StartEmulator(BeBoardFWInterface* pInterface)
@@ -51,21 +43,13 @@ namespace Ph2_HwInterface
   {
     pInterface->WriteReg( "fc7_daq_ctrl.physical_interface_block.fe_for_ps_roh.stop_fe_for_ps_roh_data_player", 0x01 );
     std::this_thread::sleep_for (std::chrono::milliseconds (500) );
-    
-    if( pInterface->ReadReg("fc7_daq_stat.physical_interface_block.fe_for_ps_roh.stat_fe_for_ps_roh_data_player") == 1 )
-    {
-      fEmulatorRunning = false;
-      LOG(INFO) << BOLDGREEN << " Data Player [STOPPED] " << RESET;
-    }
-    else{
-      LOG(INFO) << BOLDRED << " Data Player [STOP ERROR]" << RESET;
-    }
+    LOG (INFO) << BOLDBLUE << "FE data player for PS ROH stopped." << RESET;
+
   }
 
   bool ROHInterface::EmulatorIsRunning(BeBoardFWInterface* pInterface)
   {
-    if( pInterface->ReadReg("fc7_daq_stat.physical_interface_block.fe_for_ps_roh.stat_fe_for_ps_roh_data_player") )
-      fEmulatorRunning = true;
+    fEmulatorRunning = ( pInterface->ReadReg("fc7_daq_stat.physical_interface_block.fe_for_ps_roh.stat_fe_for_ps_roh_data_player") == 1 );
     return fEmulatorRunning;
   }
 }
