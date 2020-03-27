62c2eb99e102c01ae801d15b09a9f59655a2d4de
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2020-03-27 21:24:56 +0100

    ReadData now works.. I think

--- System/SystemController.cc
+++ System/SystemController.cc
@@ -435,9 +435,7 @@ namespace Ph2_System
                 LOG (DEBUG) << BOLDGREEN << "Event" << +cEventIndex << " .. Data word that should be event header ..  " << std::bitset<32>(*cEventIterator) << ". Event is made up of " << +cEventSize <<  " 32 bit words..." << RESET;
                 if( pBoard->getFrontEndType() == FrontEndType::CBC3 )
                 {
-                  fNevents   = static_cast<uint32_t>(pNevents);
                   fEventSize = static_cast<uint32_t>(cEventSize);
-
                   fNCbc = (fEventSize - D19C_EVENT_HEADER1_SIZE_32_CBC3) / D19C_EVENT_SIZE_32_CBC3 / fNFe;
                   fEventList.push_back ( new D19cCbc3Event ( pBoard, fNCbc, fNFe , cEvent ) );
                 }
@@ -458,91 +456,6 @@ namespace Ph2_System
               }
               cEventIterator += cEventSize;
             }while( cEventIterator < pData.end());
-        }
-        // moved the ZS case to here ... to be tackled afterwards
-        else
-        {
-          fNCbc = 0;
-          if( pBoard->getFrontEndType() == FrontEndType::CIC || pBoard->getFrontEndType() == FrontEndType::CIC2 )
-          {
-            auto it = pData.begin();
-            size_t cEventIndex=0;
-            bool cCondition = (pNevents > 0 ) ? (it != pData.end() && cEventIndex < pNevents) : (it != pData.end()) ;
-            while(cCondition)
-            {
-              cCondition = (pNevents > 0 ) ? (it != pData.end() && cEventIndex < pNevents) : (it != pData.end()) ;
-              uint32_t cEventSize = (0x0000FFFF & (*it))*4 ; // event size is given in 128 bit words
-              auto cEnd = ( (it+cEventSize) > pData.end() ) ? pData.end() : (it + cEventSize) ;
-              bool cCondition2 = (pNevents > 0 ) ?  (cEnd <= pData.end() && cEventIndex < pNevents) : (cEnd <= pData.end());
-              if( cCondition2 )
-              {
-                // retrieve chunck of data vector belonging to this event
-                std::vector<uint32_t> cEvent(it, cEnd);
-
-                // some useful debug information
-                LOG (INFO) << BOLDGREEN << "Event" << +cEventIndex << " : " << std::bitset<32>(cEvent[0]) << " : " << +cEventSize <<  " 32 bit words." << RESET;
-                // push back event into event list
-                LOG (DEBUG) << BOLDBLUE << "CIC events..." << RESET;
-                for( auto cWord : cEvent )
-                {
-                  LOG (INFO) << BOLDBLUE << "\t.... " << std::bitset<32>(cWord) << RESET;
-
-                }
-                //fEventList.push_back ( new D19cCicEvent ( pBoard, fNCbc , fNFe, cEvent ) );
-                cEventIndex++;
-              }
-              it = cEnd;
-            }
-          }
-          else
-          {
-            fNCbc = 0;
-            //use a SwapIndex to decide wether to swap a word or not
-            //use a WordIndex to pick events apart
-            uint32_t cWordIndex = 0;
-            uint32_t cSwapIndex = 0;
-            // index of the word inside the event (ZS)
-            uint32_t fZSEventSize = 0;
-            uint32_t cZSWordIndex = 0;
-
-            // to fill fEventList
-            std::vector<uint32_t> lvec;
-            for ( auto word : pData )
-            {
-                //if the SwapIndex is greater than 0 and a multiple of the event size in 32 bit words, reset SwapIndex to 0
-                if (cSwapIndex > 0 && cSwapIndex % fEventSize == 0) cSwapIndex = 0;
-
-                  #ifdef __CBCDAQ_DEV__
-                            //TODO
-                            LOG (DEBUG) << std::setw (3) << "Original " << cWordIndex << " ### " << std::bitset<32> (pData.at (cWordIndex) );
-                            if ( (cWordIndex + 1) % fEventSize == 0 && cWordIndex > 0 ) LOG (DEBUG) << std::endl << std::endl;
-                  #endif
-                lvec.push_back ( word );
-                if ( cZSWordIndex == fZSEventSize - 1 )
-                {
-                    //LOG(INFO) << "Packing event # " << fEventList.size() << ", Event size is " << fZSEventSize << " words";
-                    if (pType == BoardType::D19C)
-                        fEventList.push_back ( new D19cCbc3EventZS ( pBoard, fZSEventSize, lvec ) );
-                    lvec.clear();
-                    if (fEventList.size() >= fNevents) break;
-                }
-                else if ( cZSWordIndex == fZSEventSize )
-                {
-                    // get next event size
-                    cZSWordIndex = 0;
-                    if (pType == BoardType::D19C)
-                        fZSEventSize = (0x0000FFFF & word);
-                    if (fZSEventSize > pData.size() )
-                    {
-                        LOG (ERROR) << "Missaligned data, not accepted";
-                        break;
-                    }
-                }
-                cWordIndex++;
-                cSwapIndex++;
-                cZSWordIndex++;
-            }
-          }
         }
         LOG (INFO) << BOLDBLUE << "Completed event decoding.." << RESET;
 
