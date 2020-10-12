3a461dfa02ef7cb0a0de3e7e777bb3afd45ab40e
Mauro Dinardo <mauro.dinardo@cern.ch>
2020-10-12 12:04:52 +0200

    Minor big fix in state machine configuration

--- HWInterface/RD53FWInterface.cc
+++ HWInterface/RD53FWInterface.cc
@@ -1231,8 +1231,8 @@ void RD53FWInterface::SetAndConfigureFastCommands(const BeBoard* pBoard, size_t 
         RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_first_prime = 0;
         RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_ecr         = 0;
         RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_inject      = 0;
-        RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_trigger     = INJdelay::BeforePrimeCal;
-        RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_prime       = (nClkDelays == 0 ? (uint32_t)INJdelay::Loop : nClkDelays);
+        RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_trigger     = (nClkDelays == 0 ? (uint32_t)INJdelay::Loop : nClkDelays);
+        RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.delay_after_prime       = 0;
 
         RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.first_cal_en  = false;
         RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.second_cal_en = false;
