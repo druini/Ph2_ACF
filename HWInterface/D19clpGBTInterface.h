abd6939203eda207ba261e990f3ae241653058d5
Younes Otarid <yotarid@cern.ch>
2021-03-26 15:52:31 +0100

    adding some lpGBT status maps

--- HWInterface/D19clpGBTInterface.h
+++ HWInterface/D19clpGBTInterface.h
@@ -203,7 +203,26 @@ class D19clpGBTInterface : public lpGBTInterface
                                                    {"TEMP", 14},
                                                    {"VREF/2", 15}};
 
-    std::map<uint8_t, std::string> fI2CStatusMap = {{4, "TransactionSucess"}, {8, "SDAPulledLow"}, {32, "InvalidCommand"}, {64, "NotACK"}};
+    std::map<uint8_t, std::string> fPUSMStatusMap = {{0, "ARESET"},
+                                                     {1, "RESET"},
+                                                     {2, "WAIT_VDD_STABLE"},
+                                                     {3, "WAIT_VDD_HIGHER_THAN_0V90"},
+                                                     {4, "FUSE_SAMPLING"},
+                                                     {5, "UPDATE_FROM_FUSES"},
+                                                     {6, "WAIT_FOR_PLL_CONFIG"},
+                                                     {7, "WAIT_POWER_GOOD"},
+                                                     {8, "RESETOUT"},
+                                                     {9, "I2C_TRANS"},
+                                                     {10, "RESET_PLL"},
+                                                     {11, "WAIT_PLL_LOCK"},
+                                                     {12, "INIT_SCRAM"},
+                                                     {13, "PAUSE_FOR_DLL_CONFIG"},
+                                                     {14, "RESET_DLLS"},
+                                                     {15, "WAIT_DLL_LOCK"},
+                                                     {16, "RESET_LOGIC_USING_DLL"},
+                                                     {17, "WAIT_CHNS_LOCKED"},
+                                                     {18, "READY"}};
+    std::map<uint8_t, std::string> fI2CStatusMap  = {{4, "TransactionSucess"}, {8, "SDAPulledLow"}, {32, "InvalidCommand"}, {64, "NotACK"}};
 
     std::map<uint8_t, std::string> fEOMStatusMap = {{0, "smIdle"}, {1, "smResetCounters"}, {2, "smCount"}, {3, "smEndOfCount"}};
 
