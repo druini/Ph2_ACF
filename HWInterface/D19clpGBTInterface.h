e6209110f394c7bf38fa0586ce5c31e3d5835225
Younes Otarid <yotarid@cern.ch>
2021-03-26 15:53:22 +0100

    overriding BER and PRBS functions (needed for OT??)

--- HWInterface/D19clpGBTInterface.h
+++ HWInterface/D19clpGBTInterface.h
@@ -37,9 +37,12 @@ class D19clpGBTInterface : public lpGBTInterface
     bool     WriteChipReg(Ph2_HwDescription::Chip* pChip, const std::string& pRegNode, uint16_t pValue, bool pVerifLoop = true) override;
     uint16_t ReadChipReg(Ph2_HwDescription::Chip* pChip, const std::string& pRegNode) override;
     // R/W functions using register address
-    bool     WriteReg(Ph2_HwDescription::Chip* pChip, uint16_t pAddress, uint16_t pValue, bool pVerifLoop = false);
+    bool     WriteReg(Ph2_HwDescription::Chip* pChip, uint16_t pAddress, uint16_t pValue, bool pVerifLoop = true);
     uint16_t ReadReg(Ph2_HwDescription::Chip* pChip, uint16_t pAddress);
     bool     WriteChipMultReg(Ph2_HwDescription::Chip* pChip, const std::vector<std::pair<std::string, uint16_t>>& RegVec, bool pVerifLoop = true) override;
+    bool     RunBERtest(Ph2_HwDescription::Chip* pChip, uint8_t pGroup, uint8_t pChannel, bool given_time, double frames_or_time, uint8_t frontendSpeed = 0) override {return true;};
+    void     StartPRBSpattern(Ph2_HwDescription::Chip* pChip) override {};
+    void     StopPRBSpattern(Ph2_HwDescription::Chip* pChip) override {};
 
     // #######################################
     // # LpGBT block configuration functions #
