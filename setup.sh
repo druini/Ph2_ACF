8eb5277dbaac22e87365c81e7fc44bf7699c06b3
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2021-04-23 14:09:54 +0200

    setting up in Aachen

--- setup.sh
+++ setup.sh
@@ -3,20 +3,20 @@
 ##########
 # CACTUS #
 ##########
-export CACTUSBIN=/opt/cactus/bin
-export CACTUSLIB=/opt/cactus/lib
-export CACTUSINCLUDE=/opt/cactus/include
-export CACTUSROOT=/opt/cactus/
-#export CACTUSROOT=/opt/cactus_from_source/uhal/
+#export CACTUSBIN=/opt/cactus/bin
+#export CACTUSLIB=/opt/cactus/lib
+#export CACTUSINCLUDE=/opt/cactus/include
+#export CACTUSROOT=/opt/cactus/
+export CACTUSROOT=/opt/cactus_from_source/uhal/
 
-#export CACTUSINCLUDE=$CACTUSROOT/uhal/include/
-#export CACTUSLIB=$CACTUSROOT/uhal/lib
+export CACTUSINCLUDE=$CACTUSROOT/uhal/include/
+export CACTUSLIB=$CACTUSROOT/uhal/lib
 
-#export CACTUSLOGINCLUDE=$CACTUSROOT/log/include/
-#export CACTUSLOGLIB=$CACTUSROOT/log/lib/
+export CACTUSLOGINCLUDE=$CACTUSROOT/log/include/
+export CACTUSLOGLIB=$CACTUSROOT/log/lib/
 
-#export CACTUSGRAMMARINCLUDE=$CACTUSROOT/grammars/include/
-#export CACTUSGRAMMARLIB=$CACTUSROOT/grammars/lib/
+export CACTUSGRAMMARINCLUDE=$CACTUSROOT/grammars/include/
+export CACTUSGRAMMARLIB=$CACTUSROOT/grammars/lib/
 
 #########
 # BOOST #
@@ -128,7 +128,7 @@ export CompileForShep=false
 export CompileWithEUDAQ=false
 
 # Compile with TC_USB library
-export CompileWithTCUSB=false
+export CompileWithTCUSB=true
 export UseTCUSBforROH=false
 
 # Clang-format command
