f7c31de225a0f49a1244ce96251e71fbf19f5093
Lesya Horyn <lesya.horyn@cern.ch>
2021-06-16 14:55:58 -0500

    Added enable flag

--- Utils/Container.h
+++ Utils/Container.h
@@ -29,7 +29,7 @@ class ChipContainer;
 class BaseContainer
 {
   public:
-    BaseContainer(uint16_t id = -1) : id_(id), index_(0) { ; }
+    BaseContainer(uint16_t id = -1) : id_(id), index_(0), isEnabled_(true) { ; }
 
     BaseContainer(const BaseContainer&) = delete;
     BaseContainer(BaseContainer&& theCopyContainer)
@@ -43,12 +43,15 @@ class BaseContainer
     uint16_t               getIndex(void) const { return index_; }
     virtual void           cleanDataStored(void)            = 0;
     virtual BaseContainer* getElement(uint16_t index) const = 0;
+    bool                   isEnabled() const {return isEnabled_;}
+    void                   setEnabled(bool enable) {isEnabled_ = enable;}
 
     void setIndex(uint16_t index) { index_ = index; }
 
   private:
     uint16_t id_;
     uint16_t index_;
+    bool     isEnabled_;
 };
 
 template <class T>
