a0df7caab64b8e93bdc640ff71a85c527d5aa562
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2021-08-04 19:10:32 +0200

    This one is for Sarah The greatest ever

--- tools/SEHMonitor.cc
+++ tools/SEHMonitor.cc
@@ -15,18 +15,17 @@ void SEHMonitor::runMonitor()
 void SEHMonitor::runInputCurrentMonitor()
 {
     LOG(INFO) << BOLDMAGENTA << "Running Input Current Monitor" << RESET;
-    
-    LOG(INFO) << BOLDMAGENTA << "We pretend to be a measurement" << RESET;
-        
-        // for(const auto& board: *theSystCntr.fDetectorContainer)
-        // {
-        //     for(const auto& opticalGroup: *board)
-        //     {
 
-        // for(const auto& hybrid: *opticalGroup)
-        // {
-        // uint16_t cbcOrMpa = theSystCntr.fCicInterface->ReadChipReg(static_cast<const Ph2_HwDescription::OuterTrackerHybrid*>(hybrid)->fCic, "CBCMPA_SEL"); // just to read something
-        //     LOG(INFO) << BOLDMAGENTA << "Hybrid " << hybrid->getId() << " - CBCMPA_SEL = " << cbcOrMpa << RESET;
-        // }
-    
+    LOG(INFO) << BOLDMAGENTA << "We pretend to be a measurement" << RESET;
+
+    // for(const auto& board: *theSystCntr.fDetectorContainer)
+    // {
+    //     for(const auto& opticalGroup: *board)
+    //     {
+
+    // for(const auto& hybrid: *opticalGroup)
+    // {
+    // uint16_t cbcOrMpa = theSystCntr.fCicInterface->ReadChipReg(static_cast<const Ph2_HwDescription::OuterTrackerHybrid*>(hybrid)->fCic, "CBCMPA_SEL"); // just to read something
+    //     LOG(INFO) << BOLDMAGENTA << "Hybrid " << hybrid->getId() << " - CBCMPA_SEL = " << cbcOrMpa << RESET;
+    // }
 }
