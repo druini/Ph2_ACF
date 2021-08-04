dbfff8ab0c1c4e8bda392a79dc24a671190ebc1e
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2021-08-04 15:21:21 +0200

    please dont ask

--- tools/SEHMonitor.cc
+++ tools/SEHMonitor.cc
@@ -15,16 +15,9 @@ void SEHMonitor::runMonitor()
 void SEHMonitor::runInputCurrentMonitor()
 {
     LOG(INFO) << BOLDMAGENTA << "Running Input Current Monitor" << RESET;
-    for(const auto& board: *theSystCntr.fDetectorContainer)
-    {
-        if(board->at(0)->flpGBT == nullptr) { continue; }
-        for(const auto& opticalGroup: *board)
-        {
-            // D19clpGBTInterface* clpGBTInterface = static_cast<D19clpGBTInterface*>(theSystCntr.flpGBTInterface);
-            if(false) { static_cast<D19clpGBTInterface*>(theSystCntr.flpGBTInterface)->ReadADC(opticalGroup->flpGBT, "ADC5"); }
-            LOG(INFO) << BOLDMAGENTA << "We pretend to be a measurement" << RESET;
-        }
-
+    
+    LOG(INFO) << BOLDMAGENTA << "We pretend to be a measurement" << RESET;
+        
         // for(const auto& board: *theSystCntr.fDetectorContainer)
         // {
         //     for(const auto& opticalGroup: *board)
@@ -35,5 +28,5 @@ void SEHMonitor::runInputCurrentMonitor()
         // uint16_t cbcOrMpa = theSystCntr.fCicInterface->ReadChipReg(static_cast<const Ph2_HwDescription::OuterTrackerHybrid*>(hybrid)->fCic, "CBCMPA_SEL"); // just to read something
         //     LOG(INFO) << BOLDMAGENTA << "Hybrid " << hybrid->getId() << " - CBCMPA_SEL = " << cbcOrMpa << RESET;
         // }
-    }
+    
 }
