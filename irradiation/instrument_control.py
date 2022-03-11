f4bd191719a944ed5a2f12096d365aa2e18f8d7d
druini <daniele.ruini@cern.ch>
2022-03-11 13:54:40 +0100

    adding power_cycle to power supply, and MultimeterController

--- irradiation/instrument_control.py
+++ irradiation/instrument_control.py
@@ -1,5 +1,10 @@
-from icicle.tti import TTI
+'''
+Instrument control based on the icicle library: https://gitlab.cern.ch/ethz-phase2-pixels/icicle
+'''
 
+from icicle.tti import TTI
+from icicle.keithley2000 import Keithley2000
+import time
 
 class PowerSupplyController:
     def __init__(self, resource, outputs):
@@ -25,6 +30,24 @@ class PowerSupplyController:
         with self.tti:
             return self.tti.set_channel('CURRENT', channel, value)
 
-    def read_current():
+    def read_current(self, channel):
         with self.tti:
             return self.tti.measure(channel)[1]
+
+    def power_cycle():
+        with self.tti:
+            self.tti.off('ALL')
+            time.sleep(.5)
+            return self.tti.on('ALL')
+
+class MultimeterController:
+    def __init__(self, resource):
+        self.keithley2000 = Keithley2000(resource=resource)
+        with self.keithley2000:
+            self.keithley2000.set_all_line_integrations(1)
+
+    def measure_voltage(self):
+        with self.keithley2000 as multimeter:
+            if (multimeter.query('CONFIGURE', no_lock=True).strip('"') != 'VOLT:DC' or int(multimeter.query('INITIALISE_CONTINUOUS', no_lock=True)) == 1):
+                multimeter.set('CONFIGURE', 'VOLT:DC', no_lock=True)
+            return next(multimeter.measure('VOLT:DC'))
