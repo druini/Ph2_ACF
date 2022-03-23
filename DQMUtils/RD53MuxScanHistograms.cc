31a5b1b330c0d07b253feb49adf96449390f38a4
Daniele Ruini <daniele.ruini@cern.ch>
2022-03-23 15:56:21 +0100

    update vmux names to croc (by Umberto)

--- DQMUtils/RD53MuxScanHistograms.cc
+++ DQMUtils/RD53MuxScanHistograms.cc
@@ -19,13 +19,13 @@ using namespace Ph2_HwInterface;
 void MuxScanHistograms::fillMUX( const std::vector<double>& VMUXvolt, const std::vector<double>& VMUXvolt_ADC, const std::vector<double>& IMUXvolt, const std::vector<double>& IMUXvolt_ADC, int run_counter )
 {
 	//Need to be kept here because they are not seen in header file (investigate)
-	static const std::vector<std::string> VMUXVar = {"Vref_ADC","NTC","VCAL_DAC_half","VDDA_half","Poly_TEMPSENS_top","Poly_TEMPSENS_bottom","VCAL_HI","VCAL_MED","DIFF_FE_VTH2","DIFF_FE_VTH1_Main_array",
-		"DIFF_FE_VTH1_Left","DIFF_FE_VTH1_Right","RADSENS_Analog_SLDO","TEMPSENS_Analog_SLDO","RADSENS_Digital_SLDO","TEMPSENS_Digital_SLDO","RADSENS_center","TEMPSENS_center","Analog_GND","NONE","NONE","NONE",
-		"NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","Vref_CORE","Vref_PRE","VINA_half","VDDA_half","VrefA","VOFS_quarter","VIND_quarter","VDDD_half","VrefD"};
+	static const std::vector<std::string> VMUXVar = {"Vref_ADC","NTC_pad_voltage","Vref_CAL_DAC_half","VDDA_half","Poly_TEMPSENS_top","Poly_TEMPSENS_bottom","VCAL_HI","VCAL_MED","Lin_FE_Ref_KRUM",
+		"LIN_FE_GDAC_Main","LIN_FE_GDAC_Left","LIN_FE_GDAC_Right","RADSENS_Analog_SLDO","TEMPSENS_Analog_SLDO","RADSENS_Digital_SLDO","TEMPSENS_Digital_SLDO","RADSENS_center","TEMPSENS_center","Analog_GND",
+		"NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","Vref_CORE","Vref_PRE","VINA_quarter","VDDA_half","VrefA","VOFS_quarter","VIND_quarter","VDDD_half","VrefD"};
 	static const std::vector<std::string> IMUXVar = {"IREF","CDR_VCO_main_bias","CDR_VCO_buffer_bias","CDR_CP_current","CDR_FD_current","CDR_buffer_bias","CML_driver_tap_2_bias","CML_driver_tap_1_bias",
-		"CML_driver_main_bias","NTC_pad_current","Capmeasure_circuit","Capmeasure_parasitic","DIFF_FE_Preamp_Main_array","DIFF_FE_PreComp","DIFF_FE_Comparator","DIFF_FE_VTH2","DIFF_FE_VTH1_Main_array",
-		"DIFF_FE_LCC","DIFF_FE_Feedback","DIFF_FE_Preamp_Left","DIFF_FE_VTH1_Left","DIFF_FE_Preamp_Right","DIFF_FE Preamp Top-Left","DIFF_FE VTH1 Right","DIFF_FE Preamp Top","DIFF_FE Preamp Top-Right","NONE",
-		"NONE","Analog_input_current","Analog_shunt_current","Digital_input_current","Digital_shunt_current"};
+		"CML_driver_main_bias","NTC_pad_current","Capmeasure_circuit","Capmeasure_parasitic","LIN_FE_Preamp_Main","LIN_FE_CompsTAR","LIN_FE_Comparator","LIN_FE_LDAC","LIN_DE_FC", "LIN_DE_KrumCurr","None",
+		"LIN_FE_Preamp_Left","None","LIN_FE_Preamp_Right","LIN_FE_Preamp_Top-Left","None","LIN_FE_Preamp_Top","LIN_FE_Preamp_Top-Right","NONE","NONE","Analog_input_current","Analog_shunt_current",
+		"Digital_input_current","Digital_shunt_current"};
 	static const std::string ADCString = "_ADC";
     //if ( run_counter == 0 )
 	//	remove( "Results/muxScan.root" ); //Remove old file if it's not part of the current run
