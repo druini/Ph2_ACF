0b80f75f04516c2a18c05b16d75bf5f2b5d925f3
Daniele Ruini <daniele.ruini@cern.ch>
2022-04-01 14:30:28 +0200

    improve preIrrad routine

--- irradiation/scan_routine_config.py
+++ irradiation/scan_routine_config.py
@@ -1,82 +1,107 @@
 config_preIrradiation = [
-    #{
-    #    "name": "VrefTrimming",
-    #    "type": "Ph2_ACF",
-    #    "configFile": "CROC.xml",
-    #    "updateConfig" : True,
-    #    "tools": ["VrefTrimming"],
-    #    "params": [
-    #        {
-    #            "table" : "Pixels",
-    #            "keys" : ["tdac"],
-    #            "values" : [16]
-    #        },
-    #    ]
-    #},
-    #{
-    #    'name': 'current_vs_PA_IN_BIAS_LIN',
-    #    'type': 'curr_vs_DAC',
-    #    'configFile': 'CROC.xml',
-    #    'PSchannel': 2,
-    #    'params': [
-    #        {
-    #            'table': 'Registers',
-    #            'keys' : ['DAC_PREAMP_L_LIN', 'DAC_PREAMP_R_LIN', 'DAC_PREAMP_TL_LIN', 'DAC_PREAMP_TR_LIN', 'DAC_PREAMP_T_LIN', 'DAC_PREAMP_M_LIN'],
-    #            'values' : [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000]
-    #        },
-    #    ]
-    #},
+    {
+        "name": "VrefTrimming",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : True,
+        "tools": ["VrefTrimming"],
+        "params": [
+            {
+                "table" : "Pixels",
+                "keys" : ["tdac"],
+                "values" : [16]
+            },
+        ]
+    },
+    {
+        "name": "vmonitor",
+        "type": "Vmonitor",
+    },
     ##{
-    ##    "name": "ThresholdTuning1000",
-    ##    "type": "Ph2_ACF",
-    ##    "configFile": "CROC.xml",
-    ##    "updateConfig" : True,
-    ##    "tools": ["ThresholdEqualization3000", "GlobalThresholdTuning1000", "ThresholdEqualization1000", "ThresholdScanSparse"],
-    ##        {
-    ##            "table" : "Registers",
-    ##            "keys" : ["DAC_PREAMP_L_LIN", "DAC_PREAMP_R_LIN", "DAC_PREAMP_TL_LIN", "DAC_PREAMP_TR_LIN", "DAC_PREAMP_T_LIN", "DAC_PREAMP_M_LIN"],
-    ##            "values" : [348]
-    ##        },
-    ##},
-    ##{
-    ##    "name": "ThresholdScanSparse",
-    ##    "type": "Ph2_ACF",
-    ##    "configFile": "CROC.xml",
-    ##    "tools": ["ThresholdScanSparse"],
-    ##    "updateConfig" : False,
-    ##},
-    ##{
-    ##    "name": "ToT_vs_KRUM_CURR_LIN",
-    ##    "type": "Ph2_ACF",
-    ##    "configFile": "CROC.xml",
-    ##    "updateConfig" : True,
-    ##    "tools": ["AnalogScanSparse"],
+    ##    'name': 'current_vs_PA_IN_BIAS_LIN',
+    ##    'type': 'curr_vs_DAC',
+    ##    'configFile': 'CROC.xml',
+    ##    'PSchannel': 2,
     ##    'params': [
     ##        {
     ##            'table': 'Registers',
-    ##            'keys' : ['VCAL_HIGH'],
-    ##            'values' : [1391]
-    ##        },
-    ##        {
-    ##            'table': 'Registers',
-    ##            'keys' : ['DAC_KRUM_CURR_LIN'],
-    ##            'values' : [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200]
+    ##            'keys' : ['DAC_PREAMP_L_LIN', 'DAC_PREAMP_R_LIN', 'DAC_PREAMP_TL_LIN', 'DAC_PREAMP_TR_LIN', 'DAC_PREAMP_T_LIN', 'DAC_PREAMP_M_LIN'],
+    ##            'values' : [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000]
     ##        },
     ##    ]
     ##},
     {
+        "name": "MaskStuckUntuned",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : True,
+        "tools": ["StuckPixelScan"],
+        "params": [
+            {
+                "table" : "Pixels",
+                "keys" : ["enable"],
+                "values" : [1]
+            },
+            {
+                "table" : "Pixels",
+                "keys" : ["tdac"],
+                "values" : [16]
+            },
+            {
+                "table" : "Registers",
+                "keys" : ["DAC_GDAC_L_LIN", "DAC_GDAC_R_LIN", "DAC_GDAC_M_LIN"],
+                "values" : [470]
+            },
+        ]
+    },
+    {
+        "name": "MaskNoisyUntuned",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : True,
+        "tools": ["NoiseScan"]
+    },
+    {
         "name": "GlobalThresholdTuning3000",
         "type": "Ph2_ACF",
         "configFile": "CROC.xml",
-        "tools": ["GlobalThresholdTuning3000", "ThresholdScanSparse"],
+        "tools": ["GlobalThresholdTuning3000"],
         "updateConfig" : True,
         "params": [
             {
                 "table" : "Pixels",
                 "keys" : ["tdac"],
                 "values" : [16]
-            }
+            },
         ]
+    },
+    {
+        "name": "MaskStuck3000",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : True,
+        "tools": ["StuckPixelScan"],
+        "params": [
+            {
+                "table" : "Pixels",
+                "keys" : ["enable"],
+                "values" : [1]
+            },
+        ]
+    },
+    {
+        "name": "MaskNoisy3000",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : True,
+        "tools": ["NoiseScan"]
+    },
+    {
+        "name": "ThresholdScan3000Single",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "tools": ["ThresholdScanSparse"],
+        "updateConfig" : False,
     },
     {
         "name": "BasicScans3000",
@@ -121,7 +146,35 @@ config_preIrradiation = [
         "type": "Ph2_ACF",
         "configFile": "CROC.xml",
         "updateConfig" : True,
-        "tools": ["ThresholdEqualization3000", "GlobalThresholdTuning1000", "ThresholdEqualization1000", "GlobalThresholdTuning1000", "ThresholdScanLow"]
+        "tools": ["ThresholdEqualization3000", "GlobalThresholdTuning1000", "ThresholdEqualization1000", "GlobalThresholdTuning1000", "ThresholdScanLow"],
+        "params": [
+            {
+                "table" : "Pixels",
+                "keys" : ["enable"],
+                "values" : [1]
+            },
+        ]
+    },
+    {
+        "name": "GainTuning1000",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : True,
+        "tools": ["GainTuning"],
+    },
+    {
+        "name": "ToTMeasurement1000",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : False,
+        "tools": ["AnalogScan"],
+        "params": [
+            {
+                "table" : "Registers",
+                "keys" : ["VCAL_HIGH"],
+                "values" : [1391]
+            },
+        ]
     },
     {
         "name": "MaskStuck1000",
@@ -136,13 +189,6 @@ config_preIrradiation = [
         "configFile": "CROC.xml",
         "updateConfig" : True,
         "tools": ["NoiseScan"]
-    },
-    {
-        "name": "GainTuning1000",
-        "type": "Ph2_ACF",
-        "configFile": "CROC.xml",
-        "updateConfig" : True,
-        "tools": ["GainTuning"],
     },
     {
         "name": "NoiseScan1000",
@@ -194,6 +240,7 @@ config_irradiationBase = [
         "name": "ShortRingOsc_Mux",
         "type": "Ph2_ACF",
         "configFile": "CROC.xml",
+        "updateConfig" : False,
         "tools": ["ShortRingOsc", "MuxScan"],
     },
     {
@@ -237,7 +284,7 @@ config_irradiationMain = [
         "type": "Ph2_ACF",
         "configFile": "CROC.xml",
         "updateConfig" : False,
-        "tools": ["ThresholdScan"],
+        "tools": ["ThresholdScanLow"],
         "params": [
             {
                 "table" : "Pixels",
@@ -259,6 +306,20 @@ config_irradiationMain = [
         "configFile": "CROC.xml",
         "updateConfig" : True,
         "tools": ["GainTuning"],
+    },
+    {
+        "name": "ToTMeasurement1000",
+        "type": "Ph2_ACF",
+        "configFile": "CROC.xml",
+        "updateConfig" : False,
+        "tools": ["AnalogScan"],
+        "params": [
+            {
+                "table" : "Registers",
+                "keys" : ["VCAL_HIGH"],
+                "values" : [1391]
+            },
+        ]
     },
     {
         "name": "BasicScans1000",
